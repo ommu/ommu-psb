29a50caee1d46c0ab147c2b3d1fc0424f8462e0a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-27 15:59:14 +0700

    setting: source

---
+++ modules/psb/views/o/setting/admin_delete.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Psb Settings (psb-settings)
+ * @var $this SettingController
+ * @var $model PsbSettings
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 27 April 2016, 12:11 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Psb Settings'=>array('manage'),
+		'Delete',
+	);
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'psb-settings-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+	<div class="dialog-content">
+		<?php echo Yii::t('phrase', 'Are you sure you want to delete this item?');?>	</div>
+	<div class="dialog-submit">
+		<?php echo CHtml::submitButton(Yii::t('phrase', 'Delete'), array('onclick' => 'setEnableSave()')); ?>
+		<?php echo CHtml::button(Yii::t('phrase', 'Cancel'), array('id'=>'closed')); ?>
+	</div>
+	
+<?php $this->endWidget(); ?>
