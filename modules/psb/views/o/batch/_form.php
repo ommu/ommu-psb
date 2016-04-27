991d81529300d6edd900f19fbf5e019d021e86a2
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-27 16:02:02 +0700

    batch: commit

--- modules/psb/views/o/batch/_form.php
+++ modules/psb/views/o/batch/_form.php
@@ -97,6 +97,16 @@
 			</div>
 		</div>
 
+		<div class="clearfix publish">
+			<?php echo $form->labelEx($model,'publish'); ?>
+			<div class="desc">
+				<?php echo $form->checkBox($model,'publish'); ?>
+				<?php echo $form->labelEx($model,'publish'); ?>
+				<?php echo $form->error($model,'publish'); ?>
+				<?php /*<div class="small-px silent"></div>*/?>
+			</div>
+		</div>
+
 	</fieldset>
 </div>
 <div class="dialog-submit">
