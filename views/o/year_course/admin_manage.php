c906a2ea33ac16cdd12d29aa702a82ef5e084e6a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 18:24:46 +0700

    modify author

--- views/o/year_course/admin_manage.php
+++ views/o/year_course/admin_manage.php
@@ -4,7 +4,7 @@
  * @var $this YearcourseController
  * @var $model PsbYearCourse
  *
- * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @author Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2016 Ommu Platform (www.ommu.co)
  * @link https://github.com/ommu/ommu-psb
